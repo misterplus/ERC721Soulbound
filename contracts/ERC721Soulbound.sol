47236f68f1e387c46e83b4918a2d785c937b9baf
misterplus <whiteplayz45@gmail.com>
2022-02-04 20:03:33 +0800

    Remove unused imports

--- contracts/ERC721Soulbound.sol
+++ contracts/ERC721Soulbound.sol
@@ -3,8 +3,6 @@ pragma solidity ^0.8.11;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/utils/Context.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "./brightid/BrightIDRegistry.sol";
 
